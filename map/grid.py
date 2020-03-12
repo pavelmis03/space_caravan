3384d459ce92609db37c0c34f7063b33094e03a2
Baharev Nikita <baharevuniversal@gmail.com>
2020-03-12 23:49:00 +0300

    rename methods

--- map/grid.py
+++ map/grid.py
@@ -1,3 +1,4 @@
+
 from drawable_objects.base import DrawableObject
 
 from geometry.point import Point
@@ -5,7 +6,7 @@ from geometry.point import Point
 from controller.controller import Controller
 from scenes.base import Scene
 
-from map.gridCoordManager import GridCoordManager
+from map.gridIndexManager import GridIndexManager
 
 class Grid(DrawableObject):
     """
@@ -26,8 +27,10 @@ class Grid(DrawableObject):
         super().__init__(scene, controller, pos)
 
         self.arr = [[default_value] * width for i in range(height)]
-
-        self.coord_manager = GridCoordManager(self, self.pos, cell_width, cell_height)
+        """
+        расчет индексов по позиции делегирован index_manager
+        """
+        self.index_manager = GridIndexManager(self, self.pos, cell_width, cell_height)
 
     def process_draw(self):
         """
@@ -36,24 +39,32 @@ class Grid(DrawableObject):
         :return:
         """
         relative_center = self.scene.relative_center
-        index_i, index_j = self.coord_manager.get_coord_of_objects_on_screen(relative_center)
+        index_i, index_j = self.index_manager.get_index_of_objects_on_screen(relative_center)
 
         for i in range(index_i['min'], index_i['max']):
             for j in range(index_j['min'], index_j['max']):
                 self.arr[i][j].process_draw()
 
     def process_logic(self):
+        """
+        у статических объектов нет process_logic
+        :return:
+        """
         pass
 
+    @property
+    def cell_width(self):
+        return self.index_manager.cell_width
+    @property
+    def cell_height(self):
+        return self.index_manager.cell_height
+
     def print_arr(self):
+        """
+        для отладки
+        :return:
+        """
         for i in range(len(self.arr)):
             for j in range(len(self.arr[i])):
                 print(self.arr[i][j], end='')
             print()
-
-    @property
-    def cell_width(self):
-        return self.coord_manager.cell_width
-    @property
-    def cell_height(self):
-        return self.coord_manager.cell_height
