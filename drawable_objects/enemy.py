c28e92b2d7a180e41b41afce99dc1ddfd95d40b1
Кильдишев Александр <king3ludey@gmail.com>
2020-03-16 05:08:32 +0300

    Enemy now is approaching player without any grid interactions

--- drawable_objects/enemy.py
+++ drawable_objects/enemy.py
@@ -8,30 +8,31 @@ from geometry.point import Point
 from scenes.base import Scene
 
 
-class Bullet(Humanoid):
+class Enemy(Humanoid):
 
     IMAGE_ZOOM = 0.3
 
-    def direction_calculation(self, angle: float):
-        x_speed = math.cos(angle) * self.speed
-        y_speed = -math.sin(angle) * self.speed
+    def direction_calculation(self):
+        x_speed = math.cos(self.angle) * self.speed
+        y_speed = -math.sin(self.angle) * self.speed
         return Point(x_speed, y_speed)
 
+    def recount_angle(self):
+        vector_to_player = self.pos - self.scene.player.pos
+        self.angle = math.atan2(vector_to_player.y, -vector_to_player.x)
 
     def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0):
-        self.speed = 10
-        self.direction = self.direction_calculation(angle)
-        self.livetime = 100
-        self.bullet_type = 'bullet'
-        super().__init__ (scene, controller, self.bullet_type, pos, angle, Bullet.IMAGE_ZOOM)
+        self.angle = angle
+        self.speed = 1
+        self.direction = self.direction_calculation()
+        self.enemy_type = 'player'
+        super().__init__ (scene, controller, self.enemy_type, pos, angle, Enemy.IMAGE_ZOOM)
 
 
     def process_logic(self):
+        self.recount_angle()
+        self.direction = self.direction_calculation()
         self.move(self.pos + self.direction)
-        self.livetime -= 1
-        if (self.livetime == 0):
-            self.destroy()
-            pass
 
     def collision(self):
         pass
