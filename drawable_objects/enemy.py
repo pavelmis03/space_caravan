ea2c9c6accdf249f8ede77686eb67c86ecc71d7f
Михаил Гулинкин <sutnega@gmail.com>
2020-03-16 03:17:07 +0300

    Шаблон пули для переделывания в Enemy

--- drawable_objects/enemy.py
+++ drawable_objects/enemy.py
@@ -1,9 +1,37 @@
+import math
+
+import pygame
+
+from controller.controller import Controller
 from drawable_objects.base import Humanoid
+from geometry.point import Point
+from scenes.base import Scene
 
 
-class Enemy(Humanoid):
-    def __init__(self):
-        pass
+class Bullet(Humanoid):
+
+    IMAGE_ZOOM = 0.3
+
+    def direction_calculation(self, angle: float):
+        x_speed = math.cos(angle) * self.speed
+        y_speed = -math.sin(angle) * self.speed
+        return Point(x_speed, y_speed)
+
+
+    def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0):
+        self.speed = 10
+        self.direction = self.direction_calculation(angle)
+        self.livetime = 100
+        self.bullet_type = 'bullet'
+        super().__init__ (scene, controller, self.bullet_type, pos, angle, Bullet.IMAGE_ZOOM)
+
 
     def process_logic(self):
+        self.move(self.pos + self.direction)
+        self.livetime -= 1
+        if (self.livetime == 0):
+            self.destroy()
+            pass
+
+    def collision(self):
         pass
