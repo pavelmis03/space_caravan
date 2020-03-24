471395c57996d3c53696c499dbfa7ad095742b0f
Sorochan Ilya <super.ilya.66@gmail.com>
2020-03-24 12:06:54 +0300

    Simple pause menu

--- drawable_objects/interface/pause_manager.py
+++ drawable_objects/interface/pause_manager.py
@@ -2,6 +2,7 @@ import pygame
 
 from constants import Color
 from drawable_objects.base import AbstractObject
+from drawable_objects.button import Button
 
 
 class PauseManager(AbstractObject):
@@ -15,14 +16,32 @@ class PauseManager(AbstractObject):
         self.surface.set_alpha(PauseManager.SURFACE_ALPHA)
         self.surface.fill(PauseManager.SURFACE_COLOR)
 
-        self.buttons = []
+        self.buttons = [
+            Button(self.scene, self.controller, (350, 155, 450, 195), 'Продолжить', self.unpause),
+            Button(self.scene, self.controller, (350, 205, 450, 245), 'Главное меню', self.main_menu),
+        ]
         self.active = False
 
     def process_logic(self):
         if self in self.controller.input_objects:
             if self.controller.is_key_pressed(PauseManager.OPEN):
-                self.scene.game_paused = self.active = True
+                self.pause()
+        if self.active:
+            for button in self.buttons:
+                button.process_logic()
+
+    def pause(self):
+        self.scene.game_paused = self.active = True
+
+    def unpause(self):
+        self.scene.game_paused = self.active = False
+
+    def main_menu(self):
+        self.unpause()
+        self.scene.game.set_scene(self.scene.game.MAIN_MENU_SCENE_INDEX)
 
     def process_draw(self):
         if self.active:
             self.scene.screen.blit(self.surface, (0, 0))
+            for button in self.buttons:
+                button.process_draw()
