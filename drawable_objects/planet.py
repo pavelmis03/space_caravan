78bbd140ef09bfeb834d62f3da0976cb8ec41b74
Михаил Гулинкин <sutnega@gmail.com>
2020-03-30 12:56:51 +0300

    scenes order

--- drawable_objects/planet.py
+++ drawable_objects/planet.py
@@ -40,4 +40,4 @@ class Planet(SpriteObject):
     def process_logic(self):
         click_pos = self.controller.get_click_pos()
         if self.controller.is_key_pressed(key=pygame.K_e):
-            self.scene.game.set_scene(self.scene.game.SPACEMAP_SCENE_INDEX)
+            self.scene.game.set_scene(self.scene.game.MAIN_SCENE_INDEX)
