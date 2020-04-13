65bd169492888e2462a92dae61bdc5d3b0ce70f5
Михаил Гулинкин <sutnega@gmail.com>
2020-04-13 10:24:28 +0300

    merged spaceshipScene

--- drawable_objects/space_map_terminal.py
+++ drawable_objects/space_map_terminal.py
@@ -8,10 +8,10 @@ from drawable_objects.player import Player
 
 class SpaceMapTerminal(UsableObject):
 
-    IMAGE_ZOOM = 0.3
+    IMAGE_ZOOM = 0.8
 
     def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0):
-        super().__init__ (scene, controller, Player.IMAGE_NAME, pos, angle, SpaceMapTerminal.IMAGE_ZOOM)
+        super().__init__ (scene, controller, 'level_objects.terminal_up', pos, angle, SpaceMapTerminal.IMAGE_ZOOM)
 
     def activate(self):
         self.scene.game.set_scene(self.scene.game.SPACEMAP_SCENE_INDEX)
