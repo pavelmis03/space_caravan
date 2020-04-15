3b4681c8903f1b30fc1336bb26d6164027da35a5
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2020-04-15 22:17:05 +0300

    Merge fails corrected

--- drawable_objects/player.py
+++ drawable_objects/player.py
@@ -35,7 +35,6 @@ class Player(Humanoid):
     SPEED = 10
 
     def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0):
-        self.time = 0
         super().__init__(scene, controller, Player.IMAGE_NAME, pos, angle, Player.IMAGE_ZOOM)
         # head - 140x126
         self.rotation_offset = [
