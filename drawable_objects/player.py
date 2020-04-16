c877415a9d562b200b789e30f45a348aa779a499
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2020-04-16 23:24:43 +0300

    Player code style improved

--- drawable_objects/player.py
+++ drawable_objects/player.py
@@ -1,16 +1,13 @@
 from typing import List
-from geometry.point import Point
 import pygame
 from drawable_objects.base import Humanoid
 from geometry.point import Point
-from geometry.vector import sign, length, normalized, polar_angle
+from geometry.vector import sign, length, normalized, polar_angle, get_min_vector
 from geometry.rectangle import Rectangle
 from geometry.distances import vector_dist_point_rect
 from constants.directions import DIRECTIONS
 from scenes.base import Scene
 from controller.controller import Controller
-from drawable_objects.bullet import create_bullet
-from weapons.weapons import Pistol
 from weapons.weapons import Shotgun
 
 
@@ -46,65 +43,65 @@ class Player(Humanoid):
         self.scene.interface_objects.append(self.weapon)
 
     def process_logic(self):
+        self._turn_to_mouse()
+        self._movement_controls()
+
+    def _turn_to_mouse(self):
+        """
+        Высичление позиции в относительных координатах и поворот к мыши (которая по умолчанию
+        в относительных координатах).
+        """
         relative_center = self.scene.relative_center
-        vector_to_mouse = self.controller.get_mouse_pos() + relative_center - self.pos
+        relative_pos = self.pos - relative_center
+        vector_to_mouse = self.controller.get_mouse_pos() - relative_pos
         self.angle = polar_angle(vector_to_mouse)
+
+    def _movement_controls(self):
+        """
+        Передвижение игрока по команде пользователя.
+        """
         velocity = Point()
         if self in self.controller.input_objects:
             for i in range(4):
                 if self.controller.is_key_pressed(Player.CONTROLS[i]):
                     velocity += DIRECTIONS[i]
             velocity *= self.SPEED
+        new_player_pos = self._pos_after_pull_from_walls(self.pos + velocity)
+        self.move(new_player_pos)
 
-        new_pos = self.go_from_walls(self.pos + velocity)
-        if sign(length(new_pos - self.pos - velocity)) != 0:
-            new_pos = self.go_from_walls(self.pos + velocity)
-        self.move(new_pos)
-
-    def go_from_walls(self, player_pos: Point) -> Point:
+    def _pos_after_pull_from_walls(self, player_pos: Point) -> Point:
         """
-        Вытаскивание игрока из стен.
-
-        :param player_pos: позиция игрока
-        :return: позиция игрока после выталкивания из стен
+        Вытаскивание игрока из стен (каждый раз при попытке передвижения по команде
+        пользователя позиция игрока корректируется: его вытаскивает из стен уровня).
         """
         result_pos = Point(player_pos.x, player_pos.y)
-        collision_rects = self.scene.grid.get_collision_rects_nearby(player_pos)
+        walls_rects = self.scene.grid.get_collision_rects_nearby(player_pos)
         while True:
-            vectors = self._get_vectors_to_intersected_rects(result_pos, collision_rects)
-            if len(vectors) == 0:
+            vectors_from_bumps = self._get_vectors_from_bumps(result_pos, walls_rects)
+            if len(vectors_from_bumps) == 0:
                 break
-            min_vector = self._get_min_vector(vectors)
+            min_vector = get_min_vector(vectors_from_bumps)
             push_vector = self._get_push_vector(min_vector)
             result_pos += push_vector
         return result_pos
 
-    def _get_push_vector(self, vector_to_wall: Point) -> Point:
+    def _get_push_vector(self, vector_from_bump: Point) -> Point:
         """
-        Получить вектор выталкивания
+        По вектору от соударения со стеной получить вектор выталкивания, на который
+        надо сместить игрока, чтобы он из этой стены вышел.
         """
-        intersection_distance = self.HITBOX_RADIUS - length(vector_to_wall)
-        direction_vector = normalized(vector_to_wall)
-        return direction_vector * intersection_distance
+        length_delta = self.HITBOX_RADIUS - length(vector_from_bump)
+        direction_vector = normalized(vector_from_bump)
+        return direction_vector * length_delta
 
-    def _get_min_vector(self, vectors: List[Point]) -> Point:
+    def _get_vectors_from_bumps(self, player_pos: Point, walls_rects: List[Rectangle]) -> List[Point]:
         """
-        Получить минимальный по длине вектор
+        Формирует список векторов от соударений игрока с прямоугольниками стен до центра игрока
+        (точкой соударения считается ближайшая к центру игрока точка прямоугольника).
         """
-        result = vectors[0]
-        for i in range(1, len(vectors)):
-            if length(vectors[i]) < length(result):
-                result = vectors[i]
-        return result
-
-    def _get_vectors_to_intersected_rects(self, pos: Point, rects: List[Rectangle]) -> List[Point]:
-        """
-        Проверяет расстояния от центра хитбокса до блоков стен, и если какое-то из них меньше радиуса, добавляет
-        в список.
-        """
-        result = []
-        for rect in rects:
-            vector_to_rect = vector_dist_point_rect(pos, rect)
-            if sign(self.HITBOX_RADIUS - length(vector_to_rect)) == 1:
-                result.append(vector_to_rect)
-        return result
+        vectors_from_bumps = []
+        for rect in walls_rects:
+            vector_from_rect = vector_dist_point_rect(player_pos, rect)
+            if sign(self.HITBOX_RADIUS - length(vector_from_rect)) == 1:
+                vectors_from_bumps.append(vector_from_rect)
+        return vectors_from_bumps
