30a87d7d882d8861c4f408bb165d2e8851b3c23f
Baharev Nikita <baharevuniversal@gmail.com>
2020-04-17 14:35:07 +0300

    refactor enemy and bug fix

--- drawable_objects/enemy.py
+++ drawable_objects/enemy.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import math
 
 from controller.controller import Controller
@@ -24,10 +25,10 @@ class MovingHumanoid(Humanoid):
         """
         self._recount_angle(pos)
         move_vector = vector_from_length_angle(self.SPEED, self.angle)
-        result = self._get_correct_move_vector(move_vector, pos)
+        result = self.__get_correct_move_vector(move_vector, pos)
         return result
 
-    def _get_correct_move_vector(self, velocity: Point, pos: Point) -> Point:
+    def __get_correct_move_vector(self, velocity: Point, pos: Point) -> Point:
         """
         Корректирует вектор velocity, на который должен сдвинуться MovingHumanoid к точке pos.
 
@@ -49,6 +50,9 @@ class MovingHumanoid(Humanoid):
 
 
 class EnemyCommand:
+    """
+    Команда, которую Enemy должен выполнить
+    """
     def __init__(self, type: str, *params):
         self.type = type
         self.params = params
@@ -62,6 +66,8 @@ class Enemy(MovingHumanoid):
     Если игрок стал находиться вне радиуса слышимости, стоит на месте.
 
     Еще нет коллизий с пулей.
+
+    Рефакторить этот код с паттерном состояние - плохая идея. Проверено на практике.
     """
     IMAGE_ZOOM = 0.3
     IMAGE_NAME = 'moving_objects.enemy2'
@@ -79,80 +85,107 @@ class Enemy(MovingHumanoid):
     def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0):
         super().__init__(scene, controller, Enemy.IMAGE_NAME, pos, angle, Enemy.IMAGE_ZOOM)
 
-        self.is_has_command = False
-        self.command = None
-        self.is_aggred = False
-        self.cooldown = 0
+        self.__command = None # None означает команду бездействия
+        self.__is_see_player = False
+        self.__is_aggred = False
+        self.__cooldown = 0
 
-        self.command_functions = {'move_to': self.command_move_to,
-                                  'shoot': self.command_shoot,
-                                  'aim': self.command_aim, }
+        self.__command_functions = {'move_to': self.__command_move_to,
+                                  'shoot': self.__command_shoot,
+                                  'aim': self.__command_aim, }
 
-    def command_move_to(self, pos: Point):
+    def process_logic(self):
+        """
+        Логика enemy. Во многом зависит от того, видит он player'а или нет.
+        """
+        self.__is_see_player = self.scene.grid.is_enemy_see_player(self)
+        self.__command_logic()
+        if self.__cooldown:
+            self.__cooldown -= 1
+
+    def __command_logic(self):
+        """
+        Логика выполнения той или иной команды, а также получение следующей
+        """
+        if self.__is_idle:
+            self.__command = self.__get_new_command()
+
+        if not self.__is_idle:
+            self.__command_functions[self.__command.type](*self.__command.params)
+
+    def __get_new_command(self) -> Optional[EnemyCommand]:
+        """
+        Получить следующую команду.
+        """
+        if self.__is_see_player:
+            self.__is_aggred = True
+            self.__cooldown = max(self.__cooldown, Enemy.DELAY_BEFORE_FIRST_SHOOT)
+            return EnemyCommand('aim')
+
+        if self.__is_aggred:
+            new_pos = self.scene.grid.get_pos_to_move(self)
+            if new_pos is not None:
+                return EnemyCommand('move_to', new_pos)
+            self.__is_aggred = False
+
+        return None
+
+    @property
+    def __is_idle(self):
+        """
+        Имеет ли команду или просто бездействует
+        """
+        return self.__command is None
+
+    def __command_move_to(self, pos: Point):
+        """
+        Команда движения к точке.
+        """
         if pos == self.pos:
-            self.is_has_command = False
+            self.__command = None
             """
-            рекурсивно вызывать command_logic может быть
-            плохой идеей.
+            рекурсивно вызывать __command_logic может быть плохой идеей. Но это простой способ выбрать следующую 
+            команду и выполнить.
             """
-            self.command_logic()
+            self.__command_logic()
             return
 
         self.move(self.pos + self._get_move_vector(pos))
 
-    def command_shoot(self):
+    def __command_shoot(self):
+        """
+        Команда выстрела в игрока
+        """
         self._recount_angle(self.scene.player.pos)
 
         create_bullet(self)
-        self.cooldown = Enemy.COOLDOWN_TIME
+        self.__cooldown = Enemy.COOLDOWN_TIME
 
-        self.command = EnemyCommand('aim')
+        self.__command = EnemyCommand('aim')
 
-    def command_aim(self):
+    def __command_aim(self):
         """
-        Возможно, следует переработать, чтобы он
-         сначала целился какое-то время, а потом делал первый выстрел.
+        Команда прицеливания в игрока.
         """
-        if self.can_shoot_now:
-            self.command = EnemyCommand('shoot')
-            self.command_logic()
+        if self.__can_shoot_now:
+            self.__command = EnemyCommand('shoot')
+            self.__command_logic()
             return
 
-        if not self.is_see_player:
-            self.is_has_command = False
-            self.command_logic()
+        if not self.__is_see_player:
+            self.__command = None
+            self.__command_logic()
             return
 
         self._recount_angle(self.scene.player.pos)
 
     @property
-    def can_shoot_now(self):
-        return self.is_see_player and not self.cooldown
+    def __can_shoot_now(self) -> bool:
+        """
+        Может ли выстрелить сейчас
+        """
+        return self.__is_see_player and not self.__cooldown
 
-    def create_new_command(self):
-        if self.can_shoot_now:
-            self.is_aggred = True
-            self.is_has_command = True
 
-            self.cooldown = Enemy.DELAY_BEFORE_FIRST_SHOOT
-            self.command = EnemyCommand('aim')
-        elif self.is_aggred:
-            new_pos = self.scene.grid.get_pos_to_move(self)
-            if new_pos is None:
-                self.is_aggred = False
-                return
-            self.is_has_command = True
-            self.command = EnemyCommand('move_to', new_pos)
 
-    def command_logic(self):
-        if not self.is_has_command:
-            self.create_new_command()
 
-        if self.is_has_command:
-            self.command_functions[self.command.type](*self.command.params)
-
-    def process_logic(self):
-        self.is_see_player = self.scene.grid.is_enemy_see_player(self)
-        self.command_logic()
-        if self.cooldown:
-            self.cooldown -= 1
