963b0d80ddb140aa414f167a2a2866fc211c35ae
Кильдишев Петр <epic.rabbit@mail.ru>
2020-04-26 05:50:21 +0300

    Some small changes, work in progress still, I guess

--- drawable_objects/UsableObject.py
+++ drawable_objects/UsableObject.py
@@ -22,20 +22,21 @@ class UsableObject(GameSprite):
     def __init__(self, scene: Scene, controller: Controller, image_name: str, pos: Point, angle: float = 0,
                  zoom: float = 1, usage_radius: float = 100):
         super().__init__(scene, controller, image_name, pos, angle, zoom)
-        # Радиус вокруг объекта, в пределах которого с ним можно взаимодействовать
-        self.usage_radius = usage_radius
 
-        #self.popping_e = PoppingE(self.scene, self.controller, self.pos, 0, self.pos, self.usage_radius)
-        #self.scene.game_objects.append(self.popping_e)
-        self.e_is_shown = False
+        self.usage_radius = usage_radius # Радиус вокруг объекта, в пределах которого с ним можно взаимодействовать
+        self.e_is_shown = False # Отображена ли всплывающая 'E'
 
     def process_logic(self):
         super().process_logic()
         if dist(self.scene.player.pos, self.pos) <= self.usage_radius:  # Проверка активации
             if not self.e_is_shown:
+                """
+                Создает всплывающую 'E', если она не создана.
+                Сделано в process_logic, чтобы 'E' рисовалось поверх UsableObject-а
+                """
                 self.e_is_shown = True
-                popping_e = PoppingE (self.scene, self.controller, self.pos + Point(0, -35), 0, self.pos, self.usage_radius)
-                self.scene.game_objects.append (popping_e)
+                popping_e = PoppingE(self.scene, self.controller, self.pos, 0, self.pos, self.usage_radius)
+                self.scene.game_objects.append(popping_e)
             if self.controller.is_key_pressed(key=UsableObject.ACTIVATION_KEY):
                 self.activate()
         else:
@@ -45,7 +46,6 @@ class UsableObject(GameSprite):
         """
         Функция, активирующаяся при взаимодействии с объектом
         """
-
         x_speed = cos(self.angle) * self.speed
         y_speed = -sin(self.angle) * self.speed
         return Point(x_speed, y_speed)
--- drawable_objects/poppingE.py
+++ drawable_objects/poppingE.py
@@ -6,18 +6,23 @@ from controller.controller import Controller
 from geometry.distances import dist
 
 from drawable_objects.player import Player
+from utils.image import ImageManager
 
 
 class PoppingE(GameSprite):
-
+    """
+    'E', всплывающая над объектами, если их можно использовать
+    """
     IMAGE_ZOOM = 0.05
 
     def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0, parent_pos: Point = Point(0, 0), parent_radius: float = 0):
+        pos += Point(0, -45)
         super().__init__(scene, controller, 'other.poppingE',
                          pos, angle, PoppingE.IMAGE_ZOOM)
-        self.parent_pos = parent_pos
-        self.parent_radius = parent_radius
+
+        self.parent_pos = parent_pos #позиция объекта, который может быть использован
+        self.parent_radius = parent_radius #радиус объекта, который может быть использован
 
     def process_logic(self):
         if dist (self.scene.player.pos, self.parent_pos) > self.parent_radius:
-            self.destroy()
+            self.destroy() #если игрок далеко, 'Е" уничтожается
