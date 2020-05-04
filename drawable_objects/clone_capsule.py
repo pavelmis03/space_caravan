cfae69136d070b64a5435fc111307f3ae36bed24
Михаил Гулинкин <sutnega@gmail.com>
2020-05-04 16:33:01 +0300

    Exit blueprint added

---
+++ drawable_objects/clone_capsule.py
@@ -0,0 +1,15 @@
+from controller.controller import Controller
+from drawable_objects.usable_object import  UsableObject
+from geometry.point import Point
+from scenes.base import Scene
+
+
+class CloneCapsule(UsableObject):
+    IMAGE_ZOOM = 0.8
+
+    def __init__(self, scene: Scene, controller: Controller, pos: Point, angle: float = 0):
+        super().__init__(scene, controller, 'level_objects.terminal_up',
+                         pos, angle, CloneCapsule.IMAGE_ZOOM)
+
+    def activate(self):
+        pass
