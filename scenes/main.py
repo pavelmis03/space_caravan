b22b369b79f45b7b03fe3cd1fc6b894d53d996c3
Михаил Гулинкин <sutnega@gmail.com>
2020-05-04 12:46:41 +0300

    fix

--- scenes/main.py
+++ scenes/main.py
@@ -1,5 +1,6 @@
 from console.console import Console
 from console.entry import Entry
+from drawable_objects.Chest import Chest
 from drawable_objects.Ladder import Ladder
 from drawable_objects.interface.pause_manager import PauseManager
 from drawable_objects.interface.player_icon import PlayerIcon
