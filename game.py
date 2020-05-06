e993cd79ee2175f65c09cfcf858b5c4d4eb314c4
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2020-05-06 14:42:16 +0300

    Class Game's code style improved

--- game.py
+++ game.py
@@ -23,49 +23,70 @@ class Game:
     SPACEMAP_SCENE_INDEX = 5
     GAMEOVER_SCENE_INDEX = 6
 
-    def __init__(self, width=1000, height=700):
-        self.size = (width, height)
-        self.screen = None
+    def __init__(self, width: int = 1000, height: int = 700):
         pygame.init()
-        self.create_window()
-        self.running = True
-
-        self.controller = Controller(self)
-        self.space = Space(self, self.controller)
+        self.size = (width, height)
+        self.__running = True
         ImageManager.load_all()
-        self.scenes = [
+
+        self.__controller = Controller(self)
+        self.__space = Space(self, self.__controller)
+        self.__scenes = [
             MainMenuScene(self),
             MainScene(self),
             SettingsMenuScene(self),
             AboutMenuScene(self),
             SpaceshipScene(self),
-            self.space.get_spacemap_scene(),
+            self.__space.get_spacemap_scene(),
         ]
-        self.current_scene = 0
+        self.__current_scene = 0
 
     @property
     def size(self) -> Tuple[int, int]:
-        return self._size
+        return self.__size
 
     @size.setter
     def size(self, value: Tuple[int, int]):
-        self._size = value
-        self.width = self._size[0]
-        self.height = self._size[1]
-        self.screen_rectangle = Rectangle(0, 0, self.width, self.height)
-        self.create_window()
+        """
+        Присвоением окну размера инициализируются все характеристики экрана.
+        """
+        self.__size = value
+        self.__width = self.__size[0]
+        self.__height = self.__size[1]
+        self.__screen_rectangle = Rectangle(0, 0, self.__width, self.__height)
+        self.__create_window()
 
-    def create_window(self):
-        self.screen = pygame.display.set_mode(self.size, pygame.RESIZABLE)
+    def __create_window(self):
+        self.__screen = pygame.display.set_mode(self.__size, pygame.RESIZABLE)
+
+    @property
+    def screen(self) -> pygame.Surface:
+        return self.__screen
+
+    @property
+    def width(self) -> int:
+        return self.__width
+
+    @property
+    def height(self) -> int:
+        return self.__height
+
+    @property
+    def screen_rectangle(self) -> Rectangle:
+        return self.__screen_rectangle
+
+    @property
+    def controller(self) -> Controller:
+        return self.__controller
 
     def set_scene(self, scene_index):
-        self.current_scene = scene_index
+        self.__current_scene = scene_index
 
     def end(self):
-        self.running = False
+        self.__running = False
 
     def main_loop(self):
-        while self.running:
-            self.controller.iteration()
-            self.scenes[self.current_scene].iteration()
+        while self.__running:
+            self.__controller.iteration()
+            self.__scenes[self.__current_scene].iteration()
         sys.exit(0)
--- space/dungeon.py
+++ space/dungeon.py
@@ -4,6 +4,7 @@ from drawable_objects.player import Player
 class Dungeon:
     def __init__(self, player: Player):
         self.__scene = None
+        self.__player = player
 
     def initialize(self, biom: str):
         pass
