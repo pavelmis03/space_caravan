e186d0d072f87990122e7f1769935680b71284b7
Sorochan Ilya <super.ilya.66@gmail.com>
2020-05-27 20:32:36 +0300

    Change some things

--- ci/build_elf.sh
+++ ci/build_elf.sh
@@ -1,3 +1,3 @@
-pyinstaller run.py -p . -F -n space_caravan --hidden-import pygame --windowed
+pyinstaller run.py -p . -F -n space_caravan --hidden-import packaging.requirements --hidden-import pkg_resources.py2_warn --windowed
 cp -r images dist/.
 cp -r sounds dist/.
--- run.py
+++ run.py
@@ -1,11 +1,5 @@
 from game import Game
 
-# this is needed for build
-try:
-    import pkg_resources.py2_warn
-except ImportError:
-    pass
-
 
 if __name__ == '__main__':
     g = Game()
