3fd33ca63c5d311b74de08fab64336742afabb48
Andrey Ponomarev <ponomarev.andr2002@gmail.com>
2020-05-27 22:37:08 +0300

    ListWidget added

--- drawable_objects/textbox.py
+++ drawable_objects/textbox.py
@@ -6,6 +6,7 @@ from scenes.base import Scene
 from controller.controller import Controller
 from drawable_objects.base import DrawableObject
 from geometry.rectangle import tuple_to_rectangle, rectangle_to_rect
+from geometry.point import Point
 from drawable_objects.text import Text
 
 
@@ -18,20 +19,31 @@ class TextBox(DrawableObject):
     def __init__(self, scene: Scene, controller: Controller, geometry: Tuple, prompt_str: str):
         self.geometry = tuple_to_rectangle(geometry)
         super().__init__(scene, controller, self.geometry.center)
-        self.font_size = round(self.geometry.height)
+        font_size = round(self.geometry.height)
         self.prompt_str = prompt_str
         self.main_str = ''
         self.prompt_text = Text(scene, self.geometry.top_left, prompt_str, self.PROMPT_TEXT_COLOR, 'left',
-                                self.FONT_NAME, self.font_size, False, False)
+                                self.FONT_NAME, font_size, False, False)
         self.main_text = Text(scene, self.geometry.top_left, '', self.MAIN_TEXT_COLOR, 'left', self.FONT_NAME,
-                              self.font_size, False, False)
+                              font_size, False, False)
 
-    def is_correct_symbol(self, symbol: str) -> bool:
-        return symbol.isalnum()
+    def is_symbol_correct(self, symbol: str) -> bool:
+        if 'a' <= symbol <= 'z':
+            return True
+        if 'а' <= symbol <= 'я':
+            return True
+        if '0' <= symbol <= '9':
+            return True
+        return False
 
     def set_main_str(self, new_main_str: str):
         self.main_str = new_main_str
         self.main_text.update_text(self.main_str)
+
+    def move(self, movement: Point):
+        self.geometry.move(movement)
+        self.main_text.pos += movement
+        self.prompt_text.pos += movement
 
     def process_logic(self):
         if self.controller.get_bumped_key():
@@ -39,7 +51,7 @@ class TextBox(DrawableObject):
                 self.set_main_str(self.main_str[0:-1])
             else:
                 symbol = chr(self.controller.bumped_key)
-                if self.is_correct_symbol(symbol):
+                if self.is_symbol_correct(symbol):
                     self.set_main_str(self.main_str + symbol)
 
     def process_draw(self):
