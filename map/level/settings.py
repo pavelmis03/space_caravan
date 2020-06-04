e4782afcb7aa47495795b367f7553dc95d59b044
Sorochan Ilya <super.ilya.66@gmail.com>
2020-06-04 10:00:31 +0300

    Enemy sprites restructure

--- map/level/settings.py
+++ map/level/settings.py
@@ -5,6 +5,7 @@ from utils.list import check_chance_list
 class LevelSettings:
     CHEST_WEAPON_DROP_CHANCE = 40
     CHEST_OTHER_DROP = [(30, 'MedKit'), (30, 'Essence'), (40, 'Fuel')]
+
     def __init__(self, floor_img: str, wall_img: str, enemy_img: Tuple[str, str], chest_imgs: Tuple[str, str],
                  enemy_weapons: List[Tuple[int, str]], chest_weapon_drop: List[Tuple[int, str]]):
         """
@@ -16,7 +17,7 @@ class LevelSettings:
         :param chest_weapon_drop: список дропа ([вероятность, название дропа]).
         сумма вероятностей должна быть равна 100
         """
-        self.level_filenames = [wall_img, floor_img] #порядок важен
+        self.level_filenames = [wall_img, floor_img]  # порядок важен
         self.enemy_img = enemy_img
         self.chest_imgs = chest_imgs
 
@@ -30,37 +31,37 @@ class LevelSettings:
 
 # список настроек для каждого уровня.
 level_settings = [
-LevelSettings('level.simple_planet.floor2', 'level.simple_planet.wall',
-                ('moving_objects.enemy_demo_rifle', 'moving_objects.enemy_with_sword'),
+    LevelSettings('level.simple_planet.floor2', 'level.simple_planet.wall',
+                  ('enemy.demo.range', 'enemy.simple.melee'),
                   ('level_objects.boxes.box_wood', 'level_objects.boxes.box_wood_open'),
-                [(60, 'Pistol'), (40, 'TwoBarrelShotgun')],
+                  [(60, 'Pistol'), (40, 'TwoBarrelShotgun')],
                   [(50, 'TwoBarrelShotgun'), (50, 'OldRifle')]),
     LevelSettings('level.simple_planet.floor2', 'level.simple_planet.wall',
-                  ('moving_objects.enemy_simple_rifle', 'moving_objects.enemy_with_sword'),
+                  ('enemy.simple.range', 'enemy.simple.melee'),
                   ('level_objects.boxes.box_wood', 'level_objects.boxes.box_wood_open'),
-                [(50, 'Sword'), (25, 'Pistol'), (25, 'TwoBarrelShotgun')],
+                  [(50, 'Sword'), (25, 'Pistol'), (25, 'TwoBarrelShotgun')],
                   [(50, 'TwoBarrelShotgun'), (50, 'OldRifle')]),
     LevelSettings('level.ice_planet.floor2', 'level.ice_planet.wall',
-                  ('moving_objects.enemy_ice_rifle', 'moving_objects.enemy_with_sword'),
-                ('level_objects.boxes.box_wood', 'level_objects.boxes.box_wood_open'),
-                [(30, 'Pistol'), (10, 'TwoBarrelShotgun'),
-                 (30, 'OldRifle'), (30, 'SemiAutomaticRifle')],
+                  ('enemy.ice.range', 'enemy.ice.melee'),
+                  ('level_objects.boxes.box_wood', 'level_objects.boxes.box_wood_open'),
+                  [(30, 'Pistol'), (10, 'TwoBarrelShotgun'),
+                   (30, 'OldRifle'), (30, 'SemiAutomaticRifle')],
                   [(40, 'SemiAutomaticRifle'), (40, 'ThreeBarrelShotgun'), (20, 'SniperRifle')]),
     LevelSettings('level.lava_planet.floor2', 'level.lava_planet.wall',
-                  ('moving_objects.enemy_lava_rifle', 'moving_objects.enemy_with_sword'),
-                ('level_objects.boxes.box', 'level_objects.boxes.box_open'),
-                [(30, 'SemiAutomaticRifle'), (30, 'ThreeBarrelShotgun'),
-                 (25, 'SniperRifle'), (15, 'BurstFiringPistol')],
+                  ('enemy.lava.range', 'enemy.lava.melee'),
+                  ('level_objects.boxes.box', 'level_objects.boxes.box_open'),
+                  [(30, 'SemiAutomaticRifle'), (30, 'ThreeBarrelShotgun'),
+                   (25, 'SniperRifle'), (15, 'BurstFiringPistol')],
                   [(16, 'SniperRifle'), (28, 'BurstFiringPistol'),
                    (28, 'Shotgun'), (28, 'AutomaticRifle')]),
     LevelSettings('level.violet_planet.floor1', 'level.violet_planet.wall',
-                  ('moving_objects.enemy_violet_rifle', 'moving_objects.enemy_with_sword'),
-                ('level_objects.boxes.box', 'level_objects.boxes.box_open'),
-                [(35, 'SniperRifle'), (35, 'Shotgun'), (30, 'AutomaticRifle')],
+                  ('enemy.violet.range', 'enemy.simple.melee'),
+                  ('level_objects.boxes.box', 'level_objects.boxes.box_open'),
+                  [(35, 'SniperRifle'), (35, 'Shotgun'), (30, 'AutomaticRifle')],
                   [(10, 'Sword'), (40, 'AutomaticRifle'), (50, 'TacticalShotgun')]),
     LevelSettings('level.mushroom_planet.floor1', 'level.mushroom_planet.wall',
-                  ('moving_objects.enemy_mushroom_rifle', 'moving_objects.enemy_with_sword'),
-                ('level_objects.boxes.box', 'level_objects.boxes.box_open'),
-                [(25, 'AutomaticRifle'), (35, 'Shotgun'), (40, 'TacticalShotgun')],
+                  ('enemy.mushroom.range', 'enemy.mushroom.melee'),
+                  ('level_objects.boxes.box', 'level_objects.boxes.box_open'),
+                  [(25, 'AutomaticRifle'), (35, 'Shotgun'), (40, 'TacticalShotgun')],
                   [(50, 'Sword'), (40, 'AutomaticRifle'), (10, 'TacticalShotgun')])
 ]
